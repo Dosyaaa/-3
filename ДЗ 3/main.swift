ec34300f4f43d75afd03f208b4065bd4787a1308
Dosyaaa <116172781+Dosyaaa@users.noreply.github.com>
2022-10-26 08:26:07 +0600

    Update main.swift

--- ДЗ 3/main.swift
+++ ДЗ 3/main.swift
@@ -8,48 +8,68 @@
 import Foundation
 
 //1-ое задание
-let city = readLine()!
-
-if city == "Бишкек"{
-    print("В Бишкеке сегодня 15 градусов, завтра будет 7 градусов, а послезавтра 0 градусов")
-}
-if city == "Чолпон-Ата"{
-    print("В Чолпон-Ате сегодня 12 градусов, завтра 17 градусов, а послезавтра 20 градусов")
-}
-if city == "Талас"{
-    print("В Таласе сегодня 7 градусов, завтра 6 градусов, а послезавтра 5 градусов")
-}
-if city == "Ош"{
-    print("В Оше сегодня 10 градусов, завтра 14 градусов, а послезавтра 21 градусов")
-}
-if city == "Джалал-Абад"{
-    print("В Джалал-Абаде сегодня 3 градуса, завтра 8 градусов, а послезавтра 10 градусов")
-}
-if city == "Нарын"{
-    print("В Нарыне сегодня 9 градусов, завтра 18 градусов, а послезавтра 27 градусов")
-}
-if city == "Баткен"{
-    print("В Баткене сегодня 1 градус, завтра - 3 градуса, а послезавтра 5 градусов")
-}
-
 print("Введите город в котором хотите узнать прогноз погоды на ближайшие 3 дня:")
 
-//2-ое задание
+let city = readLine()!
+func city(bishkek: String){
+    if city == "Бишкек"{
+        print("В Бишкеке сегодня 15 градусов, завтра будет 7 градусов, а послезавтра 0 градусов")
+    }
+}
+city(bishkek: "bishkek")
+func city(cholpon: String){
+    if city == "Чолпон-Ата"{
+        print("В Чолпон-Ате сегодня 12 градусов, завтра 17 градусов, а послезавтра 20 градусов")
+    }
+}
+city(cholpon: "cholpon")
+func city(talas: String){
+    if city == "Талас"{
+        print("В Таласе сегодня 7 градусов, завтра 6 градусов, а послезавтра 5 градусов")
+    }
+}
+city(talas: "talas")
+func city(osh: String){
+    if city == "Ош"{
+        print("В Оше сегодня 10 градусов, завтра 14 градусов, а послезавтра 21 градусов")
+    }
+}
+city(osh: "osh")
+func city(jalal: String){
+    if city == "Джалал-Абад"{
+        print("В Джалал-Абаде сегодня 3 градуса, завтра 8 градусов, а послезавтра 10 градусов")
+    }
+}
+city(jalal: "jalal")
+func city(naryn: String){
+    if city == "Нарын"{
+        print("В Нарыне сегодня 9 градусов, завтра 18 градусов, а послезавтра 27 градусов")
+    }
+}
+city(naryn: "naryn")
+func city(batken: String){
+    if city == "Баткен"{
+        print("В Баткене сегодня 1 градус, завтра - 3 градуса, а послезавтра 5 градусов")
+    }
+}
+city(batken: "batken")
+////2-ое задание
+
 var nitro = 65
 var milk = 80
 var eggs = 100
 var potato = 70
 var cheese = 200
-var Itog: Double = 0.0
-var Skidka: Double = 0.0
-var ItogString = ""
+var itog: Double = 0.0
+var skidka: Double = 0.0
+var itogString = ""
 
 func pokupka(n: Int,name: String){
-    Itog += Double(n)
-    ItogString = "\(name) - \(n)"
+    itog += Double(n)
+    itogString = "\(name) - \(n)"
 }
     func thing(name: String) -> Int{
-        
+
         if name.lowercased() == "нитро" {
             return nitro
         }else if name.lowercased() == "молоко"{
@@ -68,7 +88,7 @@ func pokupka(n: Int,name: String){
         }
     }
     print("Введите продукт который вам нужен:")
-    
+    print("Чек продуктов:нитро,молоко,яйца,картошка,сыр")
     let product1 = readLine()!
     pokupka(n: thing(name: product1), name: product1)
     let product2 = readLine()!
@@ -79,7 +99,6 @@ func pokupka(n: Int,name: String){
     pokupka(n: thing(name: product4), name: product4)
     let product5 = readLine()!
     pokupka(n: thing(name: product5), name: product5)
-Skidka = Itog * 0.05
-    let damn = "\(Itog) сом. Скидка =\(Skidka)\n Итоговая сумма = \(Itog - Skidka)"
-    print("Итоговая сумма со скидкой:\(Itog - Skidka)")
-
+skidka = itog * 0.05
+    let damn = "\(itog) сом. Скидка =\(skidka)\n Итоговая сумма = \(itog - skidka)"
+    print("Итоговая сумма со скидкой:\(itog - skidka)")
